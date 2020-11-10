b1c0a635497851e02f30b6296917eadb0aa20fd2
ikp-773 <abhijithkp773@gmail.com>
2020-11-10 11:06:00 +0530

    added license

--- lib/custom_pin_entry_field.dart
+++ lib/custom_pin_entry_field.dart
@@ -1,7 +1,180 @@
 library custom_pin_entry_field;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomPinEntryField extends StatefulWidget {
+  final String lastPin;
+  final int fields;
+  final onSubmit;
+  final fieldWidth;
+  final fontSize;
+  final isTextObscure;
+  final showFieldAsBox;
+  final InputDecoration decoration;
+
+  CustomPinEntryField(
+      {this.lastPin,
+      this.decoration,
+      this.fields: 4,
+      this.onSubmit,
+      this.fieldWidth: 40.0,
+      this.fontSize: 20.0,
+      this.isTextObscure: false,
+      this.showFieldAsBox: false})
+      : assert(fields > 0);
+
+  @override
+  State createState() {
+    return CustomPinEntryFieldState();
+  }
+}
+
+class CustomPinEntryFieldState extends State<CustomPinEntryField> {
+  List<String> _pin;
+  List<FocusNode> _focusNodes;
+  List<TextEditingController> _textControllers;
+
+  Widget textfields = Container();
+
+  @override
+  void initState() {
+    super.initState();
+    _pin = List<String>(widget.fields);
+    _focusNodes = List<FocusNode>(widget.fields);
+    _textControllers = List<TextEditingController>(widget.fields);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        if (widget.lastPin != null) {
+          for (var i = 0; i < widget.lastPin.length; i++) {
+            _pin[i] = widget.lastPin[i];
+          }
+        }
+        textfields = generateTextFields(context);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _textControllers.forEach((TextEditingController t) => t.dispose());
+    super.dispose();
+  }
+
+  Widget generateTextFields(BuildContext context) {
+    List<Widget> textFields = List.generate(widget.fields, (int i) {
+      return buildTextField(i, context);
+    });
+
+    if (_pin.first != null) {
+      FocusScope.of(context).requestFocus(_focusNodes[0]);
+    }
+
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        verticalDirection: VerticalDirection.down,
+        children: textFields);
+  }
+
+  void clearTextFields() {
+    _textControllers.forEach(
+        (TextEditingController tEditController) => tEditController.clear());
+    _pin.clear();
+  }
+
+  Widget buildTextField(int i, BuildContext context) {
+    if (_focusNodes[i] == null) {
+      _focusNodes[i] = FocusNode();
+    }
+    if (_textControllers[i] == null) {
+      _textControllers[i] = TextEditingController();
+      if (widget.lastPin != null) {
+        _textControllers[i].text = widget.lastPin[i];
+      }
+    }
+
+    _focusNodes[i].addListener(() {
+      if (_focusNodes[i].hasFocus) {}
+    });
+
+    final String lastDigit = _textControllers[i].text;
+
+    return Container(
+      height: widget.fieldWidth,
+      width: widget.fieldWidth,
+      margin: EdgeInsets.only(right: 10.0),
+      child: TextField(
+        autofocus: true,
+        controller: _textControllers[i],
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        maxLength: 1,
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xff393e58),
+            fontSize: widget.fontSize),
+        focusNode: _focusNodes[i],
+        obscureText: widget.isTextObscure,
+        decoration: widget.decoration ??
+            InputDecoration(
+              counterText: "",
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Color(0xfff9ab65),
+                ),
+              ),
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Color(0xffececec),
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Color(0xffececec),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Color(0xffececec),
+                ),
+              ),
+            ),
+        onChanged: (String str) {
+          setState(() {
+            _pin[i] = str;
+          });
+          if (i + 1 != widget.fields) {
+            _focusNodes[i].unfocus();
+            if (lastDigit != null && _pin[i] == '') {
+              FocusScope.of(context).requestFocus(_focusNodes[i - 1]);
+            } else {
+              FocusScope.of(context).requestFocus(_focusNodes[i + 1]);
+            }
+          } else {
+            _focusNodes[i].unfocus();
+            if (lastDigit != null && _pin[i] == '') {
+              FocusScope.of(context).requestFocus(_focusNodes[i - 1]);
+            }
+          }
+          if (_pin.every((String digit) => digit != null && digit != '')) {
+            widget.onSubmit(_pin.join());
+          }
+        },
+        onSubmitted: (String str) {
+          if (_pin.every((String digit) => digit != null && digit != '')) {
+            widget.onSubmit(_pin.join());
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return textfields;
+  }
 }
